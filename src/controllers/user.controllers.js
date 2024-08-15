52e771da3ff9535107e3527188054f637e7221f9
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-08-15 23:59:53 +0545

    sdf

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,56 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+
 
 const registerUser = asyncHandler( async (req, res) =>{
-        res.status(200).json({
-            messsage: "Ok"
-        })
+        
+    const {fullName, email,username, password} = req.body;
+    console.log("email : ", email);
+    if (
+        [fullname,email,username,password].some((field)=>field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
     }
-)
+
+
+    const existedUser = User.findOne({
+        $or:[{ username },{ email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with this email or username already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath ){
+        throw new ApiError(400, "Avatar Image is required")
+    }
+
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+
+    if(!avatar){
+        throw new ApiError(500, "Error while uploading avatar image")
+
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        username : username.toLowerCase(),
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+    const createdUser = await User.findById(user._id).select("-password")
+})
 
 
 export {registerUser} 
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,7 +51,7 @@ const UserSchema = new Schema({
 // to encrypt password
 UserSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
